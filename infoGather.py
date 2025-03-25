c2e460692f32cd74aace4c2f566ab29dc722662e
Allen Rodriguez <aerodr26@asu.edu>
2025-03-25 16:52:35 -0700

    Making of the models

---
+++ infoGather.py
@@ -0,0 +1,11 @@
+
+title = input("What is the the item (Title)? ")
+description = input("What is the description? ")
+price = input("What is the price? ")
+sellers_name= input("What is the sellers name? (If applicable) ")
+
+# Open the 5 bots and give them the info above
+
+# Give it this info 
+print(title, description, price, "Seller name:", sellers_name)
+
