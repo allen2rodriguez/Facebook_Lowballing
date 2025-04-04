17d96b0d195848b4f01ead9b2c58409459b2825e
Allen Rodriguez <aerodr26@asu.edu>
2025-04-03 17:40:44 -0700

    Install chrome driver

--- webScraper.py
+++ webScraper.py
@@ -1,18 +1,47 @@
-# This page will be used to gather inforomation by webscrapping the page
-import requests
-from bs4 import BeautifulSoup
+# Description: This page will be used to scrape the data off a listing on Facebook Marketplace or Offerup 
+# Author: Allen Rodriguez 
+# Version: 1.0.0
 
-page = input("Enter the link: ")
-page_to_scrap = requests.get(page)
+from selenium import webdriver # To laod the page and the data on it
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup # Parse the data 
+import time
+# Consider importing json to turn the data into JSON
 
-# Parse the HTML    
-soup = BeautifulSoup(page_to_scrap.text, 'html.parser')
-
-title = soup.find_all(class_='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x14z4hjw x3x7a5m xngnso2 x1qb5hxa x1xlr1w8 xzsf02u')
-description = soup.find_all(class_='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr40x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u')
-sellers_name = soup.find_all(class_='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x6prxxf xvq8zen x1s688f xzsf02u')
-price = soup.find_all(class_='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u')
-# If a vechile, also scrape the the "About this vehicle" information 
+def web_scrape(page):
+    # Set up the WebDriver
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
 
-print(title, description, sellers_name )
+    # Open up the page
+    driver.get(page)
+
+    time.sleep(100)
+    
+    
+    # Log into Facebook
+    # Username: class="inputtext _55r1 inputtext _1kbt inputtext _1kbt"
+    # Password: class="inputtext _55r1 inputtext _9npi inputtext _9npi"
+    
+
+    # Parse the HTML    
+    #soup = BeautifulSoup(driver.page_source, 'html.parser')
+    
+    #title = soup.find('h1') 
+    # TODO: add in price, description, sellers_name and if applicable "About this vehicle" information
+
+    driver.quit()
+    
+def main():
+    page = input("Enter the link: ")
+    web_scrape(page)
+    title = web_scrape(page)
+
+    # print(title.text)
+
+if __name__ == '__main__':
+    # This will allow the module to be run directly or imported without executing the code block below it
+    main()
+    print("Web scraping completed.")
