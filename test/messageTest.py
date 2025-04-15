c5772ab6464ba82618b5257a798698634e3dee35
Allen Rodriguez <aerodr26@asu.edu>
2025-04-15 15:27:41 -0700

    Just chugging along

--- test/messageTest.py
+++ test/messageTest.py
@@ -2,4 +2,4 @@
 
 from sendingMessages import send_message
 
-send_message("https://offerup.com/item/detail/7862b4d8-0d29-3f5d-9ddb-5dcad35f312c", "This is a test message from the bot!")
+send_message("https://offerup.com/item/detail/7862b4d8-0d29-3f5d-9ddb-5dcad35f312c", "This is a test message from the bot!", "allenr1004@gmail.com", "password123") # Replace with your own username and password
--- test/sendingMessages.py
+++ test/sendingMessages.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-def send_message(page, text):
+def send_message(page: str, text: str, username: str, password: str):
     # Set up the WebDriver
     service = Service(executable_path="chromedriver.exe")
     driver = webdriver.Chrome(service=service)
@@ -15,11 +15,37 @@ def send_message(page, text):
     # Open up the page
     driver.get(page)
 
-    # Log in 
-    # Press button 
-    # Click email
-    # Input Username and password
+    # Log in
+    button = driver.find_elements(By.TAG_NAME, "button")
+    login_button = button[9]
+    login_button.click()
 
+    # Continue with Email
+    button1 = driver.find_elements(By.TAG_NAME, "button")
+    email_button = button1[-1] # After updating the button list, this new button should be the last
+    email_button.click()
+
+    # Log in 
+    button2 = driver.find_elements(By.TAG_NAME, "button")
+    login_button = button2[-1] # After updating the button list, this new button should be the last 
+    login_button.click()
+
+    # Input Username and password
+    email_input = driver.find_element(By.NAME, "email")
+    email_input.clear()
+    email_input.send_keys(username)
+
+    password_input = driver.find_element(By.NAME, "password")
+    password_input.clear()
+    password_input.send_keys(password)
+
+    # Click the login button
+    '''
+    button3 = driver.find_elements(By.TAG_NAME, "button")
+    login_button = button3[-1] # After updating the button list, this new button should be the last
+    login_button.click()
+    '''
+    
     #Press the ask button 
     # Put it into the text area 
 
