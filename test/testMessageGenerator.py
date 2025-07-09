a138dc9597598bf729aab8457cef3ec74a0fc9dc
Allen Rodriguez <aerodr26@asu.edu>
2025-07-09 15:37:13 -0700

    Adding in tests

---
+++ test/testMessageGenerator.py
@@ -0,0 +1,8 @@
+from src.python_scripting.textGenerator import aiResponse
+
+# Test to make sure that aiResponse function and consistently return a string of the message
+# TBD: Will eventually need to make a way to input how many messages to generate
+print("Testing AI Response Generation...")
+print(aiResponse(
+    "N3 Elite Heavy Bag Punching Bag, $400, Sports Outdoors - Mixed martial arts & Boxing, 'N3 elite boxing training system. Heavy bag, punching bag, combination trainer. Free online training videos to train boxing/mma techniques, connects to wifi and you can download new videos. SharkTank invention. Also does cardio workouts. Almost new, was only used for while but my kids went off to college and i dont use it. Price OBO. Very heavy but can be broken into parts to fit in a van or suv.', David"
+    ))
