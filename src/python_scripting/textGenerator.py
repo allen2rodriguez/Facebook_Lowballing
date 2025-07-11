53cf688732a2e53857bc70a5208324b37ce3f51f
Allen Rodriguez <aerodr26@asu.edu>
2025-07-11 12:45:32 -0700

    Adding new function for selecting personas

--- src/python_scripting/textGenerator.py
+++ src/python_scripting/textGenerator.py
@@ -1,4 +1,5 @@
 import subprocess # Use this to run ollama commands
+import random
 # Consider making this into c++ (or equivalent) file to speed it up 
 def aiResponse(webScrapedInfo: str) -> str:
     try:
@@ -11,3 +12,25 @@ def aiResponse(webScrapedInfo: str) -> str:
         return ai_response
     except Exception as e:
         return None
+
+def selecting_personas (number_of_needed_personas: int) -> list:
+    '''
+    This function will select the personas that will be used to create a message
+    Args:
+        number_of_needed_personas (int): A number that indicates how many personas to select and therefore how many messages to create
+    Returns: 
+        list: The list of strings that are the selected personas
+    '''
+    da_personas = {
+        0: "The E-Girl",
+        1: "The Victim",
+        2: "The Senior Shopper",
+        3: "Prince Charming",
+        4: "Mr. Hard 2 Get",
+        5: "The Expert",
+        6: "The Douche Bag",
+        7: "The Student",
+        8: "The Reseller",
+        9: "The Collector"
+    }
+    return random.sample(list(da_personas.values()), number_of_needed_personas)
